8372e734a876b7d7586cafa2c7a5b5a166bc45aa
Zachary Miller <zachary.john.miller@gmail.com>
2024-09-28 12:21:02 -0600

    reliability changes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import express from "express";
 import http from "http";
 import { screenshotHandler } from "./screenshotHandler";
+import { asyncHandler } from "./utils/asyncHandler";
 
 dotenv.config();
 
@@ -16,7 +17,7 @@ app.use(express.json());
 
 console.log("JSON body parser enabled");
 
-app.post("/screenshot", screenshotHandler);
+app.post("/screenshot", asyncHandler(screenshotHandler));
 
 // Create HTTP backend Node API
 const server = http.createServer(app);
@@ -26,4 +27,32 @@ const PORT = Number(process.env.PORT) || 8080;
 
 server.listen(PORT, () => {
   return console.log(`Node backend is listening on ${PORT}`);
+});
+
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
+  // Optional: Perform cleanup and graceful shutdown
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+  // Optional: Perform cleanup and graceful shutdown
+});
+
+// Global Error Handler
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error("Global Error Handler:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+);
+
+// Health check endpoint
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
 });
---
+++ src/utils/asyncHandler.ts
@@ -0,0 +1,7 @@
+import { NextFunction, Request, Response } from "express";
+
+export const asyncHandler =
+  (fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
